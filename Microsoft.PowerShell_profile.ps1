7de24068e70355b86716f4afc3535612a2f816dc
nilzen <olov@nilzen.se>
2022-01-31 09:03:55 +0100

    Setup instructions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,22 @@
+# Prerequisites
+# -------------
+#
+# "-PredictionViewStyle ListView" requires PSReadLine >= 2.2.0 install module using:
+# Install-Module -Name PSReadLine -AllowPrerelease
+#
+# Create a symbolic link to this PowerShell profile script,
+# find the path to the profile script in PowerShell using:
+#
+# echo $PROFILE
+#
+# and create a symbolic link to this profile script using (CMD as administrator):
+#
+# mklink C:\path\to\Microsoft.PowerShell_profile.ps1 C:\git\dotfiles\Microsoft.PowerShell_profile.ps1
+
 oh-my-posh --init --shell pwsh --config C:\Repos\dotfiles\.ohmyposh.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
-# -PredictionViewStyle ListView Requires PSReadLine >= 2.2.0
-# Install-Module -Name PSReadLine -AllowPrerelease
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
